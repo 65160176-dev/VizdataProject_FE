2c57fc659a7d622d21c31347db83499d383a2e5e
Suphawit Rassameebunphotkul <65160307@go.buu.ac.th>
2025-12-16 11:25:51 +0700

    Gitignore

--- .nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import type { ComputedRef, MaybeRef } from 'vue'
-export type LayoutKey = "custom" | "default" | "landing"
-declare module 'nuxt/app' {
-  interface PageMeta {
-    layout?: MaybeRef<LayoutKey | false> | ComputedRef<LayoutKey | false>
-  }
-}
